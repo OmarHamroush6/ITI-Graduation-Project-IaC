71430066603bfff29e9e19cf3ab80b80c2135f62
Omar Hamroush <omarhamroush6@gmail.com>
2023-02-22 23:49:01 +0200

    infrastructure

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "omar-public" {
   associate_public_ip_address = "true"
   vpc_security_group_ids      = [aws_security_group.omar-security.id]
   subnet_id                   = aws_subnet.subnets[0].id
+  key_name                    = aws_key_pair.generated_key.key_name
   tags = {
     Name = "omar-public-ec2"
   }
--- routetables.tf
+++ routetables.tf
@@ -11,8 +11,13 @@ resource "aws_route" "public" {
   gateway_id             = aws_internet_gateway.omar_igw.id
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public-1" {
   subnet_id      = aws_subnet.subnets[0].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-2" {
+  subnet_id      = aws_subnet.subnets[1].id
   route_table_id = aws_route_table.public.id
 }
 
@@ -30,7 +35,12 @@ resource "aws_route" "private-natgw" {
   nat_gateway_id         = aws_nat_gateway.omar-GW.id
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.subnets[1].id
+resource "aws_route_table_association" "private-1" {
+  subnet_id      = aws_subnet.subnets[2].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-2" {
+  subnet_id      = aws_subnet.subnets[3].id
   route_table_id = aws_route_table.private.id
 }
